1cf871b36489948c951daa902d472c2434d49f27
codeuser-crypto <vijayshreyank@gmail.com>
2025-01-26 15:16:07 +0530

    committed

---
+++ 31-into-to-javascript-nodejs/script.js
@@ -0,0 +1,20 @@
+alert("Hello World");
+
+console.log("Code is running...")
+console.log("Code is also running...")
+console.log("Code is looking like a wow...")
+
+var a = prompt("Enter your number")
+var isTrue = confirm("Are you sure you want to leave this page and play with your computer ")
+
+if(isTrue){
+    console.log("Computer is playing")
+}
+
+else{
+    console.log("Computer is not playing")
+
+}
+console.log("Your number is " + a)
+
+document.title = "Hey I am good"
